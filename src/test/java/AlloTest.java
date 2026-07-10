ed3b7abc4032b6c76d64e4173f65a59bedee3244
Vlad <vprodd2206@gmail.com>
2026-07-10 16:14:58 +0300

    initial: setup framework and pom.xml dependencies

--- src/test/java/AlloTest.java
+++ src/test/java/AlloTest.java
@@ -3,40 +3,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AlloTest {
 
     private WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
+    @Test
+    public void testAlloLogoVisibility() throws InterruptedException {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-    }
-
-    @Test
-    public void testAlloLogoVisibility() {
         driver.get("https://allo.ua/");
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(5000);
 
         WebElement alloLogo = driver.findElement(By.xpath("//a[@class='v-logo']"));
 
         Assert.assertTrue(alloLogo.isDisplayed());
-
-        System.out.println("Тест завершився успішно: логотип знайдено і він відображається!");
-        // Створення Pull Request для перевірки ДЗ
-    }
-
-    @AfterMethod
-    public void tearDown() {
         driver.quit();
     }
 }
