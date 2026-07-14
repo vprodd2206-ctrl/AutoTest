00ae636b78d53128b49924a09ceaa7f3cc01c9f1
Vlad <vprodd2206@gmail.com>
2026-07-14 23:48:46 +0300

    feat: add test for allo search input and verify results

--- src/test/java/AlloTest.java
+++ src/test/java/AlloTest.java
@@ -22,4 +22,31 @@ public class AlloTest {
         Assert.assertTrue(alloLogo.isDisplayed());
         driver.quit();
     }
+
+    @Test
+    public void testAlloSearchFunctionality() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://allo.ua/");
+
+        Thread.sleep(5000);
+
+        WebElement searchInput = driver.findElement(By.xpath("//input[@id='search-form__input']"));
+
+        Assert.assertTrue(searchInput.isDisplayed(), "Поле пошуку не відображається на сторінці!");
+
+        searchInput.sendKeys("Фен");
+
+        WebElement searchButton = driver.findElement(By.xpath("//button[contains(@class, 'search-form__submit-button')]"));
+        searchButton.click();
+
+        Thread.sleep(5000);
+
+        WebElement firstProductTitle = driver.findElement(By.xpath("(//a[@class='product-card__title'])[1]"));
+        String productText = firstProductTitle.getText();
+
+        Assert.assertTrue(productText.toLowerCase().contains("фен"),
+                "Перший товар у списку не містить слово 'Фен'! Знайдено товар: " + productText);
+        driver.quit();
+    }
 }
