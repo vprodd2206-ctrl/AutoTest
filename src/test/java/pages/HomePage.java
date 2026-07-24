10d59fdaa5824e1b0fd31d969caa970b93f03825
Vlad <vprodd2206@gmail.com>
2026-07-24 13:57:35 +0300

    feat: add test for AirPods Pro search using Page Object

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -11,6 +11,7 @@ public class HomePage extends BasePage {
     public HomePage(WebDriver driver) {
         super(driver);
     }
+
     private final String ALLO_LOGO = "//a[@class='v-logo']";
     private final String CATALOG_BUTTON = "//div[@class='mh-catalog-btn']";
     private final String SEARCH_FIELD = "//input[@id='search-form__input']";
@@ -30,5 +31,17 @@ public class HomePage extends BasePage {
 
     public WebElement searchButton() {
         return visibilityOfElementByXpath(SEARCH_BUTTON);
+    }
+
+    public void alloLogoDisplayed() {
+        alloLogo().isDisplayed();
+    }
+
+    public void enterValuesInSearchField(String value) {
+        searchField().sendKeys(value);
+    }
+
+    public void clickSearchButton() {
+        searchButton().click();
     }
 }
