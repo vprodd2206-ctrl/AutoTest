21ffa92beb2e347f99dfd0b54a76a689617bbf4f
Vlad <vprodd2206@gmail.com>
2026-08-19 13:04:34 +0300

    Add Allure screenshot listener

--- src/test/java/basesClass/TestInit.java
+++ src/test/java/basesClass/TestInit.java
@@ -1,39 +1,46 @@
 package basesClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
-import org.testng.ITestResult;
 import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 import java.io.ByteArrayInputStream;
 
 public class TestInit {
+
     public WebDriver driver;
+
+    @BeforeMethod
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
 
     @AfterMethod
     public void closeBrowser(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            Allure.addAttachment("Скріншот падіння",
-                    new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
-        }
 
-        // Після цього спокійно закриваємо браузер
         if (driver != null) {
+
+            if (result.getStatus() == ITestResult.FAILURE) {
+                Allure.addAttachment("Скріншот падіння сторінки",
+                        new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+            }
+
             driver.quit();
         }
-    }
-
-    @AfterMethod
-    public void closeBrowser() {
-        driver.quit();
-    }
-
-    public void openUrl(String url) {
-        driver.get(url);
     }
 }
