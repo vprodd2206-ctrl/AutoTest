5bb4f9c5c9e697fe6b502a5069faadc35ff1e389
Vlad <vprodd2206@gmail.com>
2026-08-19 12:22:00 +0300

    Add Allure screenshot listener

--- src/test/java/basesClass/TestInit.java
+++ src/test/java/basesClass/TestInit.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 @Listeners(CustomTestListener.class)
 public class TestInit {
@@ -14,7 +15,13 @@ public class TestInit {
     @BeforeMethod
     public void openBrowser() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
